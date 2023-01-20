9836cfd3ace96b85b96392ae7f2543f2c243f7d6
MarcusPeixe <marcusv.peixoto23@gmail.com>
2023-01-20 19:20:47 -0300

    Super basic test for some instructions

--- gameboy_emulator/src/main.rs
+++ gameboy_emulator/src/main.rs
@@ -1,12 +1,25 @@
-mod actions;
+pub mod actions;
 
-mod cpu;
+pub mod cpu;
+use actions::Operand16bit;
+use actions::Operand8bit;
 use cpu::CPU;
 use cpu::Flags;
 
-
-
 fn main() {
+    
+    let mut cpu: CPU = CPU::new();
+    actions::push(&mut cpu, Operand16bit::Immediate(0x1234));
+    actions::ld(&mut cpu,
+        Operand8bit::Register(cpu::Registers8bit::A),
+        Operand8bit::Immediate(0xFE));
+    actions::cp(&mut cpu, Operand8bit::Immediate(0xFF));
 
+    if cpu.get_flag(Flags::Z) {
+        println!("Equal!");
+    }
+    else {
+        println!("Not equal!");
+    }
 }
 
